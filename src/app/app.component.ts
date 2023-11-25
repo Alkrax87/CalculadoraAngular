a2d2267206e49f016c3fd01d43cef00750a745ca
Alkrax87 <thedarkdawn87gtamc@gmail.com>
2023-11-25 10:18:02 -0500

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CalculadoraAngular';
+
+  num1:number = 0
+  num2:number = 0
+  res:number = 0
+  error:string = ''
+
+  suma(){
+    this.res = this.num1 + this.num2;
+    this.error = "";
+  }
+  resta(){
+    this.res = this.num1 - this.num2;
+    this.error = "";
+  }
+  multiplicacion(){
+    this.res = this.num1 * this.num2;
+    this.error = "";
+  }
+  division(){
+    if (this.num2 == 0) {
+      this.error = "No se puede dividir entre 0";
+    } else {
+      this.res = this.num1 / this.num2;
+    }
+  }
 }
